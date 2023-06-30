b2ca1d98ee80be6aa9c70dee35632fbc7fbcf527
daoxakh <daoxakh@mailbox.org>
2023-06-30 22:49:15 +0100

    init
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...

---
+++ flake.nix
@@ -0,0 +1,18 @@
+# Based on https://github.com/cmm/flake-lib/blob/master/flake.nix
+{
+  outputs = {...}: {
+    lib.addInfoPath = nixpkgs: attr:
+      let
+        lib = nixpkgs.lib;
+        allInputs = attr.buildInputs ++ attr.nativeBuildInputs;
+        drvWithInfo = lib.filter (lib.hasAttr "info") allInputs;
+        infoOutputs = map (d: d.info) drvWithInfo;
+        infoPaths = lib.filter lib.pathExists (map (out: "${out}/share/info") infoOutputs);
+      in
+        attr.overrideAttrs (finalAttrs: prevAttrs: {
+          shellHook = (prevAttrs.shellHook or "") + " \n" + ''
+                  export INFOPATH=${builtins.concatStringsSep ":" infoPaths}:$INFOPATH
+                  '';
+        });
+  };
+}
